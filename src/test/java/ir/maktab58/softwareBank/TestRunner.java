9b8405d932820d8b686730e4f16e69847026332a
unknown <tabansoleymani@yahoo.com>
2021-12-03 11:46:40 -0800

    update test files

---
+++ src/test/java/ir/maktab58/softwareBank/TestRunner.java
@@ -0,0 +1,22 @@
+package ir.maktab58.softwareBank.view;
+
+import ir.maktab58.softwareBank.models.BorrowTest;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+
+/**
+ * @author Taban Soleymani
+ */
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("ir.maktab58.service")
+public class TestRunner {
+
+}
